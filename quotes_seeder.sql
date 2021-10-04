af01057a4708ed36de4526f9851e2f5650c5029d
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-04 09:48:23 -0500

    finished INSERT lecture. completing mini exercise on INSERT

---
+++ quotes_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+INSERT INTO quotes (author_first_name, author_last_name, content) VALUES
+('1First', '1Last', '1Quote'),
+('2First', '2Last', '2Quote'),
+('3First', '3Last', '3Quote'),
+('4First', '4Last', '4Quote'),
+('5First', '5Last', '5Quote'),
+('6First', '6Last', '6Quote'),
+('7First', '7Last', '7Quote');
+
+SELECT * FROM quotes;
