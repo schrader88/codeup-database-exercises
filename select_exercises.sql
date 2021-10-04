c3b35b5267d6a2f27c3646b3bfcfe26e33077d47
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-04 10:37:41 -0500

    added one more solution to select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,7 +6,10 @@ SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club 
 
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
+# Both lines below will accomplish the same thing. Standard practice?
+
 SELECT * FROM albums WHERE release_date > 1990 && albums.release_date < 2000;
+SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 SELECT * FROM albums WHERE sales < 20;
 
