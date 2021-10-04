9b13d9b067815778bf4f2344b5f2aaddc9822786
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-04 14:56:55 -0500

    added appropriate captions to DELETE exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,10 @@
 USE codeup_test_db;
 
-
+SELECT 'Albums released after 1991' AS '=========================';
 SELECT * FROM albums WHERE release_date > 1991;
+SELECT 'Albums with Disco as genre' AS '=========================';
 SELECT * FROM albums WHERE genre = 'Disco';
+SELECT 'Album(s) by Whitney Houston' AS '=========================';
 SELECT * FROM albums WHERE artist = 'Whitney Houston / Various artists';
 
 DELETE FROM albums WHERE release_date > 1991;
