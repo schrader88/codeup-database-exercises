37c8b0d169f9a307c1f2c47cdbacbc728537e822
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-04 11:53:15 -0500

    added captions to the select and update exercises files

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,16 +1,22 @@
 USE codeup_test_db;
 
+SELECT 'Albums by Pink Floyd';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
+SELECT 'Release Date for Sgt. Pepper\'s Lonely Hearts Club Band';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
+SELECT 'Genre of Nevermind';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 
 # Both lines below will accomplish the same thing. Standard practice?
 
-SELECT * FROM albums WHERE release_date > 1990 && albums.release_date < 2000;
+SELECT 'All albums from the 1990s';
+#SELECT * FROM albums WHERE release_date > 1990 && albums.release_date < 2000;
 SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
+SELECT 'Albums that sold less than 20 Million';
 SELECT * FROM albums WHERE sales < 20;
 
+SELECT 'Albums with the genre of \'Rock\'';
 SELECT * FROM albums WHERE genre = 'Rock';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,13 +1,16 @@
 USE codeup_test_db;
 
+SELECT 'Selecting all albums';
 SELECT * FROM albums;
 UPDATE albums SET sales = (sales * 10);
 SELECT * FROM albums;
 
+SELECT 'Selecting all albums before 1980';
 SELECT * FROM albums WHERE release_date < 1980;
 UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
 SELECT * FROM albums WHERE release_date < 1980;
 
+SELECT 'Selecting all albums by Michael Jackson';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
