7bb5439120990ca97b9e85be9e6d1f73d700403b
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-05 12:40:37 -0500

    finished majority of the Functions exerciss in MySQL. first commit of assignment

---
+++ functions_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+# Exercise One
+
+SELECT CONCAT(first_name, ' ' , last_name) FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+ORDER BY emp_no;
+
+# Exercise Two
+
+SELECT first_name, last_name, birth_date FROM employees
+WHERE DAY(birth_date) = 25 AND MONTH(birth_date) = 12;
+
+# Exercise Three
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999)
+  AND (DAY(birth_date) = 25)
+  AND (MONTH(birth_date) = 12);
+
+# Exercise Four
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999)
+  AND (DAY(birth_date) = 25)
+  AND (MONTH(birth_date) = 12)
+ORDER BY hire_date DESC;
+
+# Exercise Five
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999)
+  AND (DAY(birth_date) = 25)
+  AND (MONTH(birth_date) = 12)
+ORDER BY hire_date DESC;
