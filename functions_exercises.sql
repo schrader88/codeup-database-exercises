63f0e57aff20ffd03c12303588712ea2c651331a
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-05 14:05:21 -0500

    added one more parameter to the ORDER BY statement on the last two exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -24,7 +24,7 @@ SELECT first_name, last_name, birth_date, hire_date FROM employees
 WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999)
   AND (DAY(birth_date) = 25)
   AND (MONTH(birth_date) = 12)
-ORDER BY hire_date DESC;
+ORDER BY birth_date, hire_date DESC;
 
 # Exercise Five
 
@@ -32,4 +32,4 @@ SELECT first_name, last_name, birth_date, hire_date, DATEDIFF(CURDATE(), hire_da
 WHERE (YEAR(hire_date) BETWEEN 1990 AND 1999)
   AND (DAY(birth_date) = 25)
   AND (MONTH(birth_date) = 12)
-ORDER BY hire_date DESC;
+ORDER BY birth_date, hire_date DESC;
