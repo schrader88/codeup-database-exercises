b4d0d445eeeab49eb378af2a99d481a0be89cfa6
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-07 10:37:36 -0500

    finished RIGHT JOIN lecture notes

--- join_lecture.sql
+++ join_lecture.sql
@@ -3,7 +3,7 @@ USE employees;
 SELECT COUNT(emp_no) FROM salaries;
 SELECT COUNT(emp_no) FROM employees;
 
-# JOIN or Inner JOIN:
+# JOIN or Inner JOIN, specifically Equijoin:
 
 # SELECT employees.emp_no, CONCAT(first_name, ' ',last_name) AS 'Full Name', salary, from_date, to_date FROM employees
 # JOIN salaries ON employees.emp_no = salaries.emp_no;
@@ -22,3 +22,9 @@ SELECT * FROM roles;
 # SELECT users.name, roles.name FROM roles JOIN users ON roles.id = users.role_id;
 
 SELECT users.name, roles.name FROM users LEFT JOIN roles ON roles.id = users.role_id;
+
+# RIGHT JOIN:
+# RIGHT JOIN shows rows in the right table that have no match in the left table.
+# Example: 'users RIGHT JOIN roles' - users is LEFT TABLE and roles is RIGHT TABLE
+
+SELECT users.name, roles.name FROM users RIGHT JOIN roles ON roles.id = users.role_id;
